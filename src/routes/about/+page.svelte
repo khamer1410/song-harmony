fcf3df283725c224b0b346915ff62d06e1f99ec2
Krzysztof Hamerszmit <krzysztof.hamerszmit@syzygy.pl>
2023-08-18 16:19:49 +0200

    🚀 hello butter

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,53 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+  import { UnleashClient } from 'unleash-proxy-client';
+  const TOKEN = '*:development.db0d4d2c1e72487eac8a176f99878eed5de6e98f5236a999e4a14fc4'
+
+const unleash = new UnleashClient({
+    url: 'http://localhost:4242/api/frontend',
+    clientKey: TOKEN,
+    appName: 'my-webapp',
+});
+
+window.unleash = unleash
+
+
+unleash.on('ready', () => {
+    if (unleash.isEnabled('hola')) {
+        console.log('hola feature is enabled');
+    } else {
+        console.log('hola feature is disabled');
+    }
+});
+console.log('poszło');
+
+// Start the background polling
+unleash.start();
+
+</script>
+
+<div class="text-column">
+	<h1>About this app</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
+
+	<pre>npm create svelte@latest</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
+		using it with JavaScript disabled!
+	</p>
+</div>
---
+++ src/routes/butter/+page.svelte
@@ -0,0 +1,29 @@
+<svelte:head>
+	<title>Buttera !</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+import Butter from 'buttercms';
+const butter = Butter('4c70e6ea4d3f7b58e11a604674c85560940af298');
+
+butter.page.retrieve('*', 'simple-page').then(response => console.log(response.data))
+
+
+butter.content.retrieve(['song_harmony'])
+.then(function(resp) {
+        console.log(resp.data)
+    })
+    .catch(function(resp) {
+        console.log(resp)
+    });
+
+</script>
+
+<div class="container">
+  <h1>HEllo Butter CMS</h1>
+</div>
+
+<footer>
+
+</footer>
